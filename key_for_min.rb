a95a49f365ebefd54925af2cdcda8910dcc8fab8
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-10 21:13:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
   key_for_min = nil
   lowest_value = nil
+  
 name_hash.each{|key, value|}
   if lowest_value == nil || value < lowest_value
     lowest_value = value
