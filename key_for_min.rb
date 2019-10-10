ae57296033867e7da8e52d45bcf63d5621eaf0f2
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-10 20:44:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
+def key_for_min_value(name_hash)
     i = 0
-  name_hash.each{|key, value|}
+name_hash.each{|key, value|}
   if name_hash == {}
     return nil
   elsif value = i
