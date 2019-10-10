27977dacaab87638568c674b1d90c68b9e987000
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-10 21:38:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,11 @@ def key_for_min_value(name_hash)
   key_for_min = nil
   lowest_value = nil
 
-name_hash.each{|key, value|}
+ name_hash.each do|key, value|
   if lowest_value == nil || value < lowest_value
     lowest_value = value
     key_for_min = key
   end
-return key_for_min
-  end
+ end
+key_for_min
+end
